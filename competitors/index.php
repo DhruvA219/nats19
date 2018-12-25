a971a853db12ccb500feb0c40649fa5ce17d1aed
Akash Rupela <arupela@adobe.com>
2018-12-25 07:13:53 +0530

    fix psych sheet path

--- competitors/index.php
+++ competitors/index.php
@@ -21,58 +21,58 @@ include('../header.php')
               <div class="wrapper">
                 <ul class="nav nav-tabs" role="tablist" >
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=333" ><img src="../img/events/3x3.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=333" ><img src="../img/events/3x3.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=222"><img src="../img/events/2x2.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=222"><img src="../img/events/2x2.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=444"><img src="../img/events/4x4.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=444"><img src="../img/events/4x4.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=555"><img src="../img/events/5x5.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=555"><img src="../img/events/5x5.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=666"><img src="../img/events/6x6.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=666"><img src="../img/events/6x6.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=777"><img src="../img/events/7x7.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=777"><img src="../img/events/7x7.png" class="center"></a>
 </li>
   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=333bf"><img src="../img/events/3BLD.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=333bf"><img src="../img/events/3BLD.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=333fm"><img src="../img/events/FMC.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=333fm"><img src="../img/events/FMC.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=333oh"><img src="../img/events/3OH.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=333oh"><img src="../img/events/3OH.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=333ft"><img src="../img/events/FEET.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=333ft"><img src="../img/events/FEET.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=clock"><img src="../img/events/CLOCK.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=clock"><img src="../img/events/CLOCK.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=minx" ><img src="../img/events/MINX.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=minx" ><img src="../img/events/MINX.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=pyram"><img src="../img/events/pyra.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=pyram"><img src="../img/events/pyra.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=skewb"><img src="../img/events/skewb.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=skewb"><img src="../img/events/skewb.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=sq1"><img src="../img/events/sq1.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=sq1"><img src="../img/events/sq1.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=444bf"><img src="../img/events/4BLD.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=444bf"><img src="../img/events/4BLD.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=555bf"><img src="../img/events/5BLD.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=555bf"><img src="../img/events/5BLD.png" class="center"></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="psych-sheet.php?eventId=333mbf"><img src="../img/events/MBLD.png" class="center"></a>
+                    <a class="nav-link" href="../psych-sheet?eventId=333mbf"><img src="../img/events/MBLD.png" class="center"></a>
                   </li>
                 </ul>
               </div>
