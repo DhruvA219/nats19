baa2c545e2fdb59d2eaa5311b18d405c565ac862
Akash Rupela <arupela@adobe.com>
2018-12-25 17:46:27 +0530

    header.php

--- header.php
+++ header.php
@@ -77,9 +77,16 @@ if($_GET['code'] && !isset($_SESSION['email'])) {
   <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Libraries CSS Files -->
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <script src="../lib/jquery/jquery-migrate.min.js"></script>
+  <script src="../lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../lib/easing/easing.min.js"></script>
+  <script src="../lib/superfish/hoverIntent.js"></script>
+  <script src="../lib/superfish/superfish.min.js"></script>
+  <script src="../lib/wow/wow.min.js"></script>
+  <script src="../lib/venobox/venobox.min.js"></script>
   <link href="../lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
   <link href="../lib/animate/animate.min.css" rel="stylesheet">
-  <link href="../lib/venobox/venobox.css" rel="stylesheet">
   <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
   <!-- Main Stylesheet File -->
