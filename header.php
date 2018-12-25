0ccfe2a45cd88fc3400b1fddd14f10e839d81c82
Akash Rupela <arupela@adobe.com>
2018-12-25 06:28:20 +0530

    move li out

--- header.php
+++ header.php
@@ -144,8 +144,8 @@ if($_GET['code'] && !isset($_SESSION['email'])) {
                    echo '<li class="buy-tickets"><a href=" ">';
        echo '<img src=';
        echo '"' . $_SESSION['avatar'] . '" class="signedin">';
-       echo "Welcome " . strtok($_SESSION['name'], ' ') . "</a></li>";
-       echo '<ul class="sub-nav"> <li class="sub-nav-item id = "logoutOfWebsite"><a href="../logout.php">Logout</a></li></ul>';
+       echo "Welcome " . strtok($_SESSION['name'], ' ') . "</a>";
+       echo '<ul class="sub-nav"> <li class="sub-nav-item id = "logoutOfWebsite"><a href="../logout.php">Logout</a></li></ul></li>';
      } else {
                    echo '<li class="buy-tickets">';
        echo '<a href="https://www.worldcubeassociation.org/oauth/authorize?client_id=c02ad8e3446378078c5cbb73874bac335f08d9cc36f57c74fd11f9aa6df23a7e&redirect_uri=https%3A%2F%2Fnats19.in%2F&response_type=code&scope=public+dob+email">';
