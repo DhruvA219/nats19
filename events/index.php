9168d8817273cb0a8b70992d8f984c45928804ef
Hemang Sarkar <hemangsarkar@gmail.com>
2018-12-26 18:25:37 +0000

    Adding events.

---
+++ events/index.php
@@ -0,0 +1,574 @@
+<?php
+include('../header.php')
+?>
+
+ <section id="buy-tickets" class="section-with-bg wow fadeInUp">
+		<div class="container">
+			<div class="section-header">
+				<h2>Events</h2>
+				<div class>
+				</div>
+
+</div>
+
+
+<table  class="table table-condensed show-events-table floatThead table-hover table-striped table-greedy-last-column" data-locale="en" style="table-layout: fixed; min-width: 983px;">
+<colgroup><col style="width: 144px;"><col style="width: 148px;"><col style="width: 75px;"><col style="width: 226px;"><col style="width: 181px;"><col style="width: 209px;"></colgroup>
+  <thead>
+
+  </thead><thead><tr class="size-row" style="height: 30.5px;"><th class="floatThead-col" style="height: 30.5px;"></th><th class="floatThead-col" style="height: 30.5px;"></th><th class="floatThead-col" style="height: 30.5px;"></th><th class="floatThead-col" style="height: 30.5px;"></th><th class="floatThead-col" style="height: 30.5px;"></th><th class="floatThead-col" style="height: 30.5px;"></th></tr></thead><tbody>
+<thead>
+    <tr>
+      <th>Event</th>
+      <th>Round</th>
+      <th>Format</th>
+      <th>Time limit</th>
+        <th>Cutoff</th>
+      <th>Proceed</th>
+    </tr>
+  </thead>
+       
+	<tr class="">
+          <td>
+            3x3x3 Cube
+          </td>
+          <td>First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 120 advance to next round</td>
+        </tr>
+        <tr class="">
+          <td>
+            
+          </td>
+          <td>Second round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 60 advance to next round</td>
+        </tr>
+        <tr class="">
+          <td>
+            
+          </td>
+          <td>Semi Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 16 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            2x2x2 Cube
+          </td>
+          <td>First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 90 advance to next round</td>
+        </tr>
+        <tr class="">
+          <td>
+            
+          </td>
+          <td>Second round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 20 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            4x4x4 Cube
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span> / <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            3:20.00
+          </td>
+            <td>2 attempts to get &lt; 1:20.00</td>
+          <td>Top 20 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            5x5x5 Cube
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span> / <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            5:45.00
+          </td>
+            <td>2 attempts to get &lt; 2:15.00</td>
+          <td>Top 14 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            6x6x6 Cube
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 1">Bo1</span> / <span data-toggle="tooltip" title="" data-original-title="Mean of 3">Mo3</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td>1 attempt to get &lt; 4:45.00</td>
+          <td>Top 10 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Mean of 3">Mo3</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            7x7x7 Cube
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 1">Bo1</span> / <span data-toggle="tooltip" title="" data-original-title="Mean of 3">Mo3</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td>1 attempt to get &lt; 6:30.00</td>
+          <td>Top 10 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Mean of 3">Mo3</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            3x3x3 Blindfolded
+          </td>
+          <td>First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 3">Bo3</span>
+          </td>
+          <td>
+            15:00.00 cumulative
+                <a href="#cumulative-time-limit">*</a>
+          </td>
+            <td></td>
+          <td>Top 12 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 3">Bo3</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            3x3x3 Fewest Moves
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Mean of 3">Mo3</span>
+          </td>
+          <td>
+            1 hour
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            3x3x3 One-Handed
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span> / <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            2:30.00
+          </td>
+            <td>2 attempts to get &lt; 1:00.00</td>
+          <td>Top 40 advance to next round</td>
+        </tr>
+        <tr class="">
+          <td>
+            
+          </td>
+          <td>Second round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 14 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            3x3x3 With Feet
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span> / <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            6:45.00
+          </td>
+            <td>2 attempts to get &lt; 2:45.00</td>
+          <td>Top 12 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            Clock
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span> / <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            1:15.00
+          </td>
+            <td>2 attempts to get &lt; 30.00</td>
+          <td>Top 10 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            Megaminx
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span> / <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            5:45.00
+          </td>
+            <td>2 attempts to get &lt; 2:15.00</td>
+          <td>Top 14 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            Pyraminx
+          </td>
+          <td>First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 80 advance to next round</td>
+        </tr>
+        <tr class="">
+          <td>
+            
+          </td>
+          <td>Second round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 20 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            Skewb
+          </td>
+          <td>First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 40 advance to next round</td>
+        </tr>
+        <tr class="">
+          <td>
+            
+          </td>
+          <td>Second round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td>Top 14 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="">
+          <td>
+            Square-1
+          </td>
+          <td>Combined First round</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span> / <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            2:00.00
+          </td>
+            <td>2 attempts to get &lt; 45.00</td>
+          <td>Top 12 advance to next round</td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Average of 5">Ao5</span>
+          </td>
+          <td>
+            10:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            4x4x4 Blindfolded
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 3">Bo3</span>
+          </td>
+          <td>
+            30:00.00 cumulative
+                <a href="#cumulative-time-limit">*</a>
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            5x5x5 Blindfolded
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 3">Bo3</span>
+          </td>
+          <td>
+            45:00.00 cumulative
+                <a href="#cumulative-time-limit">*</a>
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+        <tr class="last-round">
+          <td>
+            3x3x3 Multi-Blind
+          </td>
+          <td>Final</td>
+          <td>
+            <span data-toggle="tooltip" title="" data-original-title="Best of 2">Bo2</span>
+          </td>
+          <td>
+            10:00.00 per cube, up to 60:00.00
+          </td>
+            <td></td>
+          <td></td>
+        </tr>
+  </tbody>
+
+</table>
+</div>
+</section>
+
+<?php
+include('../footer.php')
+?>
