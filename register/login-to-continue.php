4de5b70b7126d5c146d469d1339d08636cde06c8
Piyush <piyushp761@users.noreply.github.com>
2018-12-26 16:03:24 +0530

    Update login-to-continue.php
    Added footer and line breaks to make it look better, hopefully doesn't break anything.

--- register/login-to-continue.php
+++ register/login-to-continue.php
@@ -1,11 +1,36 @@
 <?php
 include('../header.php')
 ?>
-
-<section id="schedule" class="section-with-bg">
-<div class="container">
-      <div class="section-header">
-        <h2>Please <u><a href="https://www.worldcubeassociation.org/oauth/authorize?client_id=c02ad8e3446378078c5cbb73874bac335f08d9cc36f57c74fd11f9aa6df23a7e&amp;redirect_uri=https%3A%2F%2Fnats19.in%2F&amp;response_type=code&amp;scope=public+dob+email">Login via WCA</a></u> to continue.</h2>
-</div>
-</div>
-</section>
+<main id="main">
+  <section id="schedule" class="section-with-bg">
+    <div class="container wow fadeInUp">
+      <div class="container">
+        <br>
+        <div class="wrapper">
+          <h2>Please <u><a href="https://www.worldcubeassociation.org/oauth/authorize?client_id=c02ad8e3446378078c5cbb73874bac335f08d9cc36f57c74fd11f9aa6df23a7e&amp;redirect_uri=https%3A%2F%2Fnats19.in%2F&amp;response_type=code&amp;scope=public+dob+email">
+            Login via WCA <img src="../img/events/wca-logo.svg" style="height: 1.1em;"></a></u> to continue.</h2>
+        </div>
+      </div>
+      <div class="tab-content row justify-content-center">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+  </div>
+  </section>
+</main>
+<?php
+include('../footer.php')
+?>
