befe0302dc26412df64f4adb0e8262afa0cae2dd
Akash Rupela <arupela@adobe.com>
2018-12-26 16:02:19 +0530

    update login to continue

--- register/login-to-continue.php
+++ register/login-to-continue.php
@@ -3,9 +3,13 @@ include('../header.php')
 ?>
 
 <section id="schedule" class="section-with-bg">
-<div class="container">
-      <div class="section-header">
-        <h2>Please <u><a href="https://www.worldcubeassociation.org/oauth/authorize?client_id=c02ad8e3446378078c5cbb73874bac335f08d9cc36f57c74fd11f9aa6df23a7e&amp;redirect_uri=https%3A%2F%2Fnats19.in%2F&amp;response_type=code&amp;scope=public+dob+email">Login via WCA</a></u> to continue.</h2>
-</div>
-</div>
+	<div class="container">
+		<div class="section-header">
+			<p align="middle">You are not logged in. Please <u><a href="https://www.worldcubeassociation.org/oauth/authorize?client_id=c02ad8e3446378078c5cbb73874bac335f08d9cc36f57c74fd11f9aa6df23a7e&amp;redirect_uri=https%3A%2F%2Fnats19.in%2F&amp;response_type=code&amp;scope=public+dob+email">Login via WCA</a></u> to continue.</p>
+		</div>
+	</div>
 </section>
+
+<?php
+include('../footer.php')
+?>
