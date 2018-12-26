70053d3ac508051a16471651d870993c221010f8
Akash Rupela <arupela@adobe.com>
2018-12-26 19:11:53 +0530

    left align names in psych sheeT

--- psych-sheet/index.php
+++ psych-sheet/index.php
@@ -83,7 +83,7 @@ include('../header.php')
             <!-- Competitors Table -->
             <div role="tabpanel" class="col-lg-9 tab-pane fade show active" id="comp">
               <div class="wrapper">
-              <table class = "striped leftcolumn1table">
+              <table class = "striped leftcolumn2table">
                 <thead>
                   <tr>
     <th> # </th>    
