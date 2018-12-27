9fa93a93ab61f6f45d8b19fea99c7586b7702540
DhruvA219 <45569769+DhruvA219@users.noreply.github.com>
2018-12-27 16:34:49 +0530

    Update index.php

--- faq/index.php
+++ faq/index.php
@@ -18,7 +18,7 @@ include('../header.php')
               <ul id="faq-list">
 
                 <li>
-                  <a data-toggle="collapse" class="collapsed" href="#faq1" aria-expanded="true">Who can register for the competition?
+                  <a data-toggle="collapse" class="collapsed" href="#faq1" aria-expanded="false">Who can register for the competition?
                       <i class="fa fa-minus-circle"></i></a>
                   <div id="faq1" class="collapse" data-parent="#faq-list">
                     <p>
@@ -28,7 +28,7 @@ include('../header.php')
                 </li>
 
                 <li>
-                  <a data-toggle="collapse" class="collapsed" href="#faq2" aria-expanded="true">Can I add more events after my registration is approved?
+                  <a data-toggle="collapse" class="collapsed" href="#faq2" aria-expanded="false">Can I add more events after my registration is approved?
                       <i class="fa fa-minus-circle"></i></a>
                   <div id="faq2" class="collapse" data-parent="#faq-list">
                     <p>
@@ -39,7 +39,7 @@ NOTE - Addition of events facility is available only before the registration per
                 </li>
 
                 <li>
-                  <a data-toggle="collapse" href="#faq3" class="collapsed" aria-expanded="true"> Can I change/remove the events I've registered for? <i class="fa fa-minus-circle"></i></a>
+                  <a data-toggle="collapse" href="#faq3" class="collapsed" aria-expanded="false"> Can I change/remove the events I've registered for? <i class="fa fa-minus-circle"></i></a>
                   <div id="faq3" class="collapse" data-parent="#faq-list">
                     <p>
                       No, changing/removing existing events is not allowed. Only new events can be added, before registration ends.
@@ -50,7 +50,7 @@ NOTE - Addition of events facility is available only before the registration per
                 </li>
 
                 <li>
-                  <a data-toggle="collapse" href="#faq4" class="collapsed" aria-expanded="true">What are the rules of this event? <i class="fa fa-minus-circle"></i></a>
+                  <a data-toggle="collapse" href="#faq4" class="collapsed" aria-expanded="false">What are the rules of this event? <i class="fa fa-minus-circle"></i></a>
                   <div id="faq4" class="collapse" data-parent="#faq-list">
                     <p>
                       The competition follows the <a class="faq-hyperlink" href="https://www.worldcubeassociation.org/regulations" target="about_blank">WCA Regulations</a>. <a class="faq-hyperlink" href="https://www.youtube.com/watch?v=dPL3eV-A0ww&" target="about_blank"> Here</a> is a video summarizing the important ones. There will be an official competitor tutorial on Saturday at 10:15 A.M. where all important rules will be explained.
@@ -59,7 +59,7 @@ NOTE - Addition of events facility is available only before the registration per
                 </li>
 
                 <li>
-                  <a data-toggle="collapse" href="#faq5" class="collapsed" aria-expanded="true"> Do I have to use my own cube to compete? <i class="fa fa-minus-circle"></i></a>
+                  <a data-toggle="collapse" href="#faq5" class="collapsed" aria-expanded="false"> Do I have to use my own cube to compete? <i class="fa fa-minus-circle"></i></a>
                   <div id="faq5" class="collapse" data-parent="#faq-list">
                     <p>
                       Yes! You are responsible for your own hardware in order to compete. This means you should bring your own puzzles.
@@ -68,7 +68,7 @@ NOTE - Addition of events facility is available only before the registration per
                 </li>
 
                 <li>
-                  <a data-toggle="collapse" href="#faq6" class="collapsed" aria-expanded="true">What is the normal age of competitors at these events? Are there age divisions?<i class="fa fa-minus-circle"></i></a>
+                  <a data-toggle="collapse" href="#faq6" class="collapsed" aria-expanded="false">What is the normal age of competitors at these events? Are there age divisions?<i class="fa fa-minus-circle"></i></a>
                   <div id="faq6" class="collapse" data-parent="#faq-list">
                     <p>
                       The competition is open to competitors of all age and gender. There will be only one combined ranking.
@@ -77,7 +77,7 @@ NOTE - Addition of events facility is available only before the registration per
                 </li>
 
                 <li>
-                  <a data-toggle="collapse" href="#faq7" class="collapsed" aria-expanded="true">How do I find the results? <i class="fa fa-minus-circle"></i></a>
+                  <a data-toggle="collapse" href="#faq7" class="collapsed" aria-expanded="false">How do I find the results? <i class="fa fa-minus-circle"></i></a>
                   <div id="faq7" class="collapse" data-parent="#faq-list">
                     <p>
                     All results are posted on the WCA website within a week after the competition finishes. However, for the day of competing, go to <a href="https://www.cubecomps.com" class="faq-hyperlink">Cubecomps</a> to find information about current events. This is also where you will find information about upcoming rounds of events. Remember, the results are entered by a human so please be patient if you do not see your results immediately.
@@ -85,7 +85,7 @@ NOTE - Addition of events facility is available only before the registration per
                   </div>
                 </li>
                 <li>
-                  <a data-toggle="collapse" href="#faq8" class="collapsed" aria-expanded="true">Can I attend the competition without competing? Do guests of competitors need to pay for the competition?
+                  <a data-toggle="collapse" href="#faq8" class="collapsed" aria-expanded="false">Can I attend the competition without competing? Do guests of competitors need to pay for the competition?
  <i class="fa fa-minus-circle"></i></a>
                   <div id="faq8" class="collapse" data-parent="#faq-list">
                     <p>
@@ -95,7 +95,7 @@ All spectators are free to attend the competition at no cost. There is payment o
                   </div>
                 </li>
                 <li>
-                  <a data-toggle="collapse" href="#faq9" class="collapsed" aria-expanded="true">Can I have fun at this event? <i class="fa fa-minus-circle"></i></a>
+                  <a data-toggle="collapse" href="#faq9" class="collapsed" aria-expanded="false">Can I have fun at this event? <i class="fa fa-minus-circle"></i></a>
                   <div id="faq9" class="collapse" data-parent="#faq-list">
                     <p>
                     More than you can imagine! Solving is just one part of the competition. There are also many unofficial events and seminars for you to have a wholesome experience.
