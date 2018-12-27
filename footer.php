a2dc21e1d05d4218b0e26ddf3ba83fb2ca862ddb
Akash Rupela <arupela@adobe.com>
2018-12-27 06:17:22 +0530

    fix footer

--- footer.php
+++ footer.php
@@ -1,65 +1,57 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-<footer id="footer">
-  <div class="footer-top">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4 col-md-6 footer-info">
-          <div class="wrapper">
-            <img src="../img/logo.png" alt="TheEvenet">
-            <p>
-              © Indian Nationals 2019 Official Website
-            </p>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 footer-info">
-          <div class="wrapper">
-            <div class="social-links" style="margin-top: 20px;">
-              <a href="https://www.worldcubeassociation.org/competitions/IndianNationals2019" target="about_blank"> <img class="footerimg" src="../img/events/wca-logo.svg"></a>
-              <a href="https://www.facebook.com/IndianNationals2019/" target="about_blank"><img class="footerimg" src="../img/events/facebook.svg"></a>
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
 
-              <a href="https://www.instagram.com/indiannationals19/" target="about_blank"><img class="footerimg" src="../img/events/instagram.svg"></a>
+          <div class="col-lg-4 col-md-4 footer-info">
+            <div class="wrapper" style="margin-top: 20px;">
+              <img src="../img/logo.png" alt="TheEvenet">
+              <p>
+                © Indian Nationals 2019 Official Website
+              </p>
+
             </div>
           </div>
-        </div>
+          <div class="col-lg-4 col-md-4 footer-info">
+            <div class="wrapper">
+              <div class="social-links" style="margin-top: 20px;">
+                <a href="https://www.worldcubeassociation.org/competitions/IndianNationals2019" target="about_blank"> <img class="footerimg" src="../img/events/wca-logo.svg"></a>
+                <a href="https://www.facebook.com/IndianNationals2019/" target="about_blank"><img class="footerimg" src="../img/events/facebook.svg"></a>
 
-
-        <div class="col-lg-4 col-md-6 footer-contact">
-          <div class="wrapper">
-            <h4>Contact Us</h4>
-
-            <div class="row">
-
-              <div class="col-lg-6 col-md-6 footer-info">
-
-                <p>
-
-                  <strong>Akash Rupela</strong> <br>
-
-                  <a href="tel:+918447852838"><i class="fas fa-phone-square fa-2x" aria-hidden="true"></i></a>
-                  <a href="mailto:arupela@worldcubeassociation.org"><i class="fas fa-envelope-square fa-2x"></i></a>
-
-                </p>
+                <a href="https://www.instagram.com/indiannationals19/" target="about_blank"><img class="footerimg" src="../img/events/instagram.svg"></a>
               </div>
-              <div class="col-lg-6 col-md-6 footer-info">
+            </div>
+          </div>
 
-                <p>
 
-                  <strong>Ujjawal Pabreja</strong> <br>
+          <div class="col-lg-4 col-md-4 footer-info">
+            <div class="wrapper">
 
-                  <a href="tel:+919818616737"><i class="fas fa-phone-square fa-2x" aria-hidden="true"></i></a>
-                  <a href="mailto:upabreja@worldcubeassociation.org"><i class="fas fa-envelope-square fa-2x"></i></a>
+              <div class="row">
 
-                </p>
+                <div class="col-lg-12 col-md-12 footer-info" style="margin-top: 20px;">
+
+
+                    <div class="contact-info">
+                      <a href="mailto:arupela@worldcubeassociation.org"><i class="fas fa-envelope-square fa-2x"></i></a> contact@nats19.in 
+                    </div>
+                    <div class="contact-info">
+                      <a href="tel:+918447852838"><i class="fas fa-phone-square fa-2x" aria-hidden="true"></i></a> (+91) 8447852838
+                    </div>
+                    <div class="contact-info">
+                      <a href="tel:+919818616737"><i class="fas fa-phone-square fa-2x" aria-hidden="true"></i></a> (+91) 9818616737
+                    </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  </footer><!-- #footer -->
+    </footer>
 </div>
 <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
 
