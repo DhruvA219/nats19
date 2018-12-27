94ca2be4f23e57f414f554bbf31406689db2d591
Akash Rupela <arupela@adobe.com>
2018-12-28 02:30:49 +0530

    make instamojo live

--- register/register.php
+++ register/register.php
@@ -134,7 +134,7 @@ if($data['success'] == 1){
    //  </script>
    //  ';
    echo '<script>
-        Instamojo.open("https://'.$mode.'.instamojo.com/@scoobydoobydoo93/'.$payment_id.'"); 
+        Instamojo.open("https://'.$mode.'.instamojo.com/@capitalcubing/'.$payment_id.'"); 
     </script>
     ';
    
