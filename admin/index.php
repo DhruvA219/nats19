4e71dd634b4e5d90bf7979c54e8457b0abefede8
Akash Rupela <arupela@adobe.com>
2019-01-02 01:16:31 +0530

    admin test v2

--- admin/index.php
+++ admin/index.php
@@ -104,12 +104,43 @@ else{ echo'<div class="wrapper">
 			echo '<td class="tg-s268">₹'.$row['price'].'</td>';	
 			echo '<td class="tg-s268">'.$row['total_quantity'].'</td>';	
 			echo '</tr>';
-			$merch_sum=$sum+($row['price']*$row['total_quantity']);
+			$merch_sum=$merch_sum+($row['price']*$row['total_quantity']);
 		}
 echo '</table> <div align="center"><b>Total amount (before gateway fee): ₹'.$merch_sum.'</b></div> </div>' ;
 
 }
 
+
+
+echo' <div class="col-lg-12 venue-info">
+							<div class="row justify-content-center" style="margin-bottom:0px; margin-top:20px;"">
+								<div class="col-11 col-lg-8">
+									<h4 align="middle"> Page Views (today)</h4>
+								</div>
+							</div>
+						</div>';
+   	  $now = new DateTime();
+      $date=$now->format('Y-m-d');
+      $get_page_visit_sql = "select * from `pageviews` where view_date='".$date."' order by count desc";
+      $page_result=$conn->query($get_page_visit_sql);
+ 	echo'<div class="wrapper">
+								<table>
+									<tr>
+										<th class="tg-s268">Page</th>
+										<th class="tg-s268">Date </th>
+										<th class="tg-s268">Views</th>
+									</tr>';
+		$views_sum=0
+		foreach($page_result as $row){
+			echo '<tr>';
+			echo '<td class="tg-s268 lefttd">'.$row['page'].'</td>';	
+			echo '<td class="tg-s268">₹'.$row['view_date'].'</td>';	
+			echo '<td class="tg-s268">'.$row['count'].'</td>';	
+			echo '</tr>';
+			$views_sum=$views_sum+$row['count'];
+		}
+echo '</table> <div align="center"><b>Total Page views today: '.$views_sum.'</b></div> </div>' ;
+                
 
 ?>
 
