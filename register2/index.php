59055619af3e817bb93d238f8c7e1c202bdfed05
Hemang Sarkar <hemangsarkar@gmail.com>
2019-01-19 19:19:35 +0000

    register2

--- register2/index.php
+++ register2/index.php
@@ -1,1 +1,23 @@
-Omegalol, testing something. Don't get angy.
+<?php
+session_start();
+
+if(!isset($_SESSION['email'])) {
+	header('location:login-to-continue.php');
+}
+
+
+include('../header.php');
+
+$current_time=time();
+$regOpenTime0=1545748037;
+$regOpenTime=1546194599;
+
+if ($current_time>$regOpenTime){
+include('registrationActual.php');
+}
+else{
+	include('registrationDisabled.php');
+}
+
+include('../footer.php');
+?>
