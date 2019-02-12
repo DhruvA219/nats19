ba3d620c8ad043d7f0a295e4b023bbf93fc933a4
Hemang Sarkar <hemangsarkar@gmail.com>
2019-02-12 17:58:43 +0000

    ujjawal commit

--- sponsors/index.php
+++ sponsors/index.php
@@ -2,6 +2,17 @@
 include('../header.php');
 
 ?>
+<style>
+.img-responsive{
+display: block;
+
+max-width: 100%;
+
+height: auto;
+}
+</style>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
   <main id="main">
     <!--==========================
@@ -12,65 +23,23 @@ include('../header.php');
         <div class="section-header">
           <h2>Sponsors</h2>
         </div>
-             <center><p> Coming soon! </p> </center>
-    <!--
-
-        <div class="row no-gutters sponsors-wrap clearfix">
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/1.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/2.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/3.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/4.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/5.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/6.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/7.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="sponsor-logo">
-              <img src="../img/sponsors/8.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-        </div>
-
-      </div> -->
 
     </section>
   </main>
+
+
+<div class="col-xs-12 col-sm-3">
+        <a href="https://www.cubelelo.com/" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/Cubelelo.png"></a>
+    </div>
+    <div class="col-xs-12 col-sm-9">
+        <h2>Cubelelo: Cubestore partner</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+            <a href="https://www.cubelelo.com/" target="_blank">Visit your Rubik's cube online store</a>!
+        </p>
+    </div>
 
 <?php
 include('../footer.php')
