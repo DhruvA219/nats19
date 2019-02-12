e04cd109482fc8813579efcdf0d93181982f7c11
Ujjawal Pabreja <ujjawalpabreja@Ujjawals-MacBook-Air.local>
2019-02-12 23:55:26 +0530

    sponsors fix

---
+++ sponsors2/index.php
@@ -0,0 +1,46 @@
+<?php
+include('../header.php');
+
+?>
+<style>
+.img-responsive{
+display: block;
+
+max-width: 100%;
+
+height: auto;
+}
+</style>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+  <main id="main">
+    <!--==========================
+      Sponsors Section
+    ============================-->
+    <section id="sponsors2" class="section-with-bg wow fadeInUp">
+         <div class="container">
+        <div class="section-header-2">
+          <h2>Sponsors</h2>
+        </div>
+
+    </section>
+  </main>
+
+
+<div class="col-xs-12 col-sm-3">
+        <a href="https://www.cubelelo.com/" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/Cubelelo.png"></a>
+    </div>
+    <div class="col-xs-12 col-sm-9">
+        <h2>Cubelelo: Cubestore partner</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+            <a href="https://www.cubelelo.com/" target="_blank">Visit your Rubik's cube online store</a>!
+        </p>
+    </div>
+
+<?php
+include('../footer.php')
+?>
