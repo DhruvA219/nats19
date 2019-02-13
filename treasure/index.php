704e6ef268b983e6b0431c90d8d0794545a15a65
DhruvA219 <45569769+DhruvA219@users.noreply.github.com>
2019-02-13 23:53:57 +0530

    Update index.php

--- treasure/index.php
+++ treasure/index.php
@@ -15,15 +15,17 @@ include('../header.php')
 
         <div class="row justify-content-center">
           <div class="col-lg-9">
-            <h3> Instructions </h3>
+            <center> <h3> Instructions </h3> </center>
             <ol>
               <li>The Treasure Hunt will start on Friday, 22nd February 00:00 IST and will end on Sunday, 24th February 23:59 IST.</li>
               <li>Top 5 participants registered for Indian Nationals will get to compete in the on-stage cubing Quiz at Indian Nationals.
               <li>Please do not discuss or share answers with anyone. Play fair to make it fun for everyone. Anyone found cheating will be disqualified.</li>
               <li>The answers are not case sensitive or whitespace sensitive. If the answer is CFOP, you can enter “CFOP” or “Cfop” or “cfop” or “C F O P” and the system will accept all of them. You have to enter the answer without quotes.</li>
-              <li>Any queries, contact us at contact@nats19.in,  our <a href="https://www.facebook.com/IndianNationals2019">Facebook page</a> or <a href="https://www.instagram.com/indiannationals19/">Instagram</a></li>
+              <li>For any queries, contact us at contact@nats19.in, our <a href="https://www.facebook.com/IndianNationals2019">Facebook page</a> or <a href="https://www.instagram.com/indiannationals19/">Instagram</a></li>
             </ol>
+            <a href="www.nats19.in/solve"><button class="tablinks">Start Tresure Hunt</button></a>
           </div>
+         
         </div>
 
       </div>
