29194f81513edac2710d82bfb844388f2850877e
DhruvA219 <45569769+DhruvA219@users.noreply.github.com>
2019-02-14 00:19:38 +0530

    Update login-to-continue.php

--- register/login-to-continue.php
+++ register/login-to-continue.php
@@ -9,7 +9,7 @@ include('../header.php')
 		<div class="section-header">
         <div class="wrapper">
 			<h2> Register </h2>
-			<p align="middle">You are not logged in. Please <u><a href="https://www.worldcubeassociation.org/oauth/authorize?client_id=c02ad8e3446378078c5cbb73874bac335f08d9cc36f57c74fd11f9aa6df23a7e&amp;redirect_uri=https%3A%2F%2Fnats19.in%2Fregister%2F&amp;response_type=code&amp;scope=public+dob+email">
+			<p align="middle">You are not logged in. Please <u><a href="https://www.worldcubeassociation.org/oauth/authorize?client_id=c02ad8e3446378078c5cbb73874bac335f08d9cc36f57c74fd11f9aa6df23a7e&amp;redirect_uri=https%3A%2F%2Fnats19.in%2F&amp;response_type=code&amp;scope=public+dob+email">
             Login via WCA <img src="../img/events/wca-logo.svg" style="height: 1.1em;"></a></u> to continue.</p>
             
         </div>
