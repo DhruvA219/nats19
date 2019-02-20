18ad9ba3670e2963b1df0867a7b2282e8277066b
Akash Rupela <arupela@adobe.com>
2019-02-21 00:15:23 +0530

    add to menu

--- header.php
+++ header.php
@@ -127,6 +127,7 @@ if($_GET['code'] && !isset($_SESSION['email'])) {
           </li>
           <li class="<?php echo ($_SERVER['PHP_SELF'] == "/events/index.php" ? "menu-active" : "");?>"><a href="../events">Events</a></li> <!-- events.html doesn't exist for now -->
           <li class="<?php echo ($_SERVER['PHP_SELF'] == "/schedule/index.php" ? "menu-active" : "");?>"><a href="../schedule">Schedule</a></li>
+          <li class="<?php echo ($_SERVER['PHP_SELF'] == "/treasure/index.php" ? "menu-active" : "");?>"><a href="../treasure">Treasure Hunt</a></li>
             <li class="<?php echo (($_SERVER['PHP_SELF'] == "/competitors/index.php" || $_SERVER['PHP_SELF'] == "/competitors/psych-sheet.php" || $_SERVER['PHP_SELF'] == "/register/index.php") ? "menu-active" : "");?>"><a href="#">Registration</a>
             <ul class="sub-nav">
                 <li class="sub-nav-item programs"><a class="sub-nav-link" href="../register">Register</a></li>
