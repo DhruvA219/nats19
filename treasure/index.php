f4b43d07233fb93103321934e33b6acf6c7a96f3
Hemang Sarkar <hemangsarkar@gmail.com>
2019-02-20 18:13:49 +0000

    Changed wording on instructions page.

--- treasure/index.php
+++ treasure/index.php
@@ -22,7 +22,7 @@ include('../header.php')
               <li>The Treasure Hunt will start on Friday, 22nd February 00:00 IST and will end on Sunday, 24th February 23:59 IST.</li>
               <li>Top 5 participants registered for Indian Nationals will get to compete in the on-stage cubing Quiz at Indian Nationals.
               <li>Please do not discuss or share answers with anyone. Play fair to make it fun for everyone. Anyone found cheating will be disqualified.</li>
-              <li>The answers are not case sensitive or whitespace sensitive. If the answer is CFOP, you can enter “CFOP” or “Cfop” or “cfop” or “C F O P” and the system will accept all of them. You have to enter the answer without quotes.</li>
+              <li>The answers are not case sensitive or whitespace sensitive. If you know the answer is CFOP, you can enter “CFOP” or “Cfop” or “cfop” or “C F O P” and the system will accept all of them. You have to enter the answer without quotes.</li>
               <li>For any queries, contact us at <a href="mailto:contact@nats19.in">contact@nats19.in</a>, our <a href="https://www.facebook.com/IndianNationals2019" target=_blank>Facebook page</a> or <a href="https://www.instagram.com/indiannationals19/" target=_blank>Instagram</a></li>
             </ol>
             <div class="tab">
