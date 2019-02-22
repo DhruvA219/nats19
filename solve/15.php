034f17157a4347b28c9672e8923ef0c83f1bdfe0
Akash Rupela <arupela@adobe.com>
2019-02-22 23:06:31 +0530

    fix yt

--- solve/15.php
+++ solve/15.php
@@ -1,9 +1,11 @@
-<p> Broadcast yourself </p>
+<p> <img src="youtube.png"> </p>
 
-<p> <i> You matter only if you are the first at something. </i> - Anonynmous 
+<p> <i> "You matter only if you are the first at something."" </i> - Anonynmous 
 </p>
+<br>
 <p>
 Ultimate - rubiks cube Presented At south Yamunanagar 5 before Years. </p>
-<p>Time? </p>
+<br>
+<p>What is the time? </p>
 
 
