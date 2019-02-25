7963493ffefbefca59a0d6a26a32f14d2f9b6f2e
Hemang Sarkar <hemangsarkar@gmail.com>
2019-02-25 05:56:33 +0000

    update

--- solve/0.php
+++ solve/0.php
@@ -1,2 +1,4 @@
-<p> Please feel free to explain in the answer box that you did not cheat or discuss answers with anyone. </p>
+<p> Please feel free to explain in the answer box that you did not cheat or discuss answers with anyone.
+Honestly tell who all did you discuss questions with or share hints? </p>
+<p style="color:red"> What did you tell them? What did they tell you? </p>
 
