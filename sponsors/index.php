dd307f8fd07dcc07522e4ae8bfeeae6ad35501e1
Akash Rupela <arupela@adobe.com>
2019-02-26 06:42:47 +0530

    spons update

--- sponsors/index.php
+++ sponsors/index.php
@@ -27,11 +27,28 @@ height: auto;
     </section>
   </main>
 
+
+
+
 <div class="row justify-content-center">
-<div class="col-xs-12 col-sm-3">
+  <div class="col-xs-12 col-sm-3">
+        <a href="https://www.cubelelo.com/" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/qiyi.png"></a>
+  </div>
+  <div class="col-xs-12 col-sm-9">
+        <h2>QiYi MoFangGe: Title Sponsor</h2>
+        <p>
+            Guangdong Fantastic Arts and Science and Education Co. Ltd. is a collective design, sales, research and development company of professional cubes, and has been committed to independent innovation, research and development design, cube culture promotion and public welfare sponsorship. "X-Man Design" and "The Valk" cover the entry-level, racing level, collection level and other innovative products. The company is one of the leading puzzle designing and manufacturing companies in the world, with a huge number of world records set using their puzzles. This is also the third year in a row that Qiyi MoFangGe has partnered with Indian Nationals as a Title Sponsor.
+        </p>
+        <p>
+            <a href="http://en.qiyitoys.net/" target="_blank">Click here to visit QiYi MoFangGe</a>!
+        </p>
+  </div>
+</div>
+<div class="row justify-content-center">
+  <div class="col-xs-12 col-sm-3">
         <a href="https://www.cubelelo.com/" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/Cubelelo.png"></a>
-    </div>
-    <div class="col-xs-12 col-sm-9">
+  </div>
+  <div class="col-xs-12 col-sm-9">
         <h2>Cubelelo: Cubestore partner</h2>
         <p>
             Founded by two cubing enthusiasts in their hostel room, cubelelo.com is the largest puzzle store in India today. Cubelelo.com is an initiative that endeavours to create a starting point for online shopping of almost all types of cubes and cube accessories featuring various brands. They offer puzzles of various categories including official cubic, non-cubic, unofficial puzzles, cube accessories and merchandise.  Their custom line of Elite cubes is preferred by many speedcubers across the country. ​ It's run by technocrats who are themselves speedcubers and always there to help you in need.
@@ -39,8 +56,22 @@ height: auto;
         <p>
             <a href="https://www.cubelelo.com/" target="_blank">Click here to visit Cubelelo</a>!
         </p>
-     </div>
-    </div>
+  </div>
+</div>
+<div class="row justify-content-center">
+  <div class="col-xs-12 col-sm-3">
+        <a href="https://www.cubelelo.com/" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/DUExpress.png"></a>
+  </div>
+  <div class="col-xs-12 col-sm-9">
+        <h2>DU Express: Media partner</h2>
+        <p>
+            DU Express is Delhi University's leading digital media house with a wide reach throughout the campus.The platform covers all the latest news,events happening in and around the campus along with publication of views,reviews & gossip which concerns the student teacher community of University of Delhi. It is one of the fastest growing college media portal and is also one of the most trusted platform of Delhi University students for their daily dose of news.
+        </p>
+        <p>
+            <a href="https://www.duexpress.in/" target="_blank">Click here to visit DU Express</a>!
+        </p>
+  </div>
+</div>
 
 <?php
 include('../footer.php')
