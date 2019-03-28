e896118654dd9e4118db323c35b9867c413bb337
Hemang Sarkar <hemangsarkar@gmail.com>
2019-03-28 00:12:36 +0000

    add more sponsor

--- sponsors/index.php
+++ sponsors/index.php
@@ -64,7 +64,35 @@ height: auto;
         </p>
   </div>
 </div>
-<<div class="row justify-content-center">
+<div class="row justify-content-center">
+  <div class="col-xs-12 col-sm-3">
+        <a href="https://www.jagran.com/" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/DainikJagran.png"></a>
+  </div>
+  <div class="col-xs-12 col-sm-9">
+        <h2>Dainik Jagran: Media partner</h2>
+        <p>
+	Jagran New Media is the digital wing of Jagran Prakashan Limited - India's leading media and communications group with its interests spanning across Print, OOH, Activations, Radio and Digital. Dainik Jagran is the flagship brand of the company. In today's dynamic media world, where consumers have an unprecedented array of choices, Dainik Jagran stands out as a brand that is the choice of millions of Indians as they start their day. With a readership of 5.59 cr, it has been the largest read daily of India for the last consecutive 21 rounds of the Indian Readership Survey (IRS). With 37 editions, Dainik Jagran covers 11 states of India. It has also been declared by the World Association of Newspapers (WAN) as the largest read daily in the world.
+	</p>
+        <p>
+            <a href="https://www.jagran.com/" target="_blank">Click here to visit Dainik Jagran</a>!
+        </p>
+  </div>
+</div>
+<div class="row justify-content-center">
+  <div class="col-xs-12 col-sm-3">
+        <a href="https://www.youtube.com/channel/UCHk5rERcW5h_4C7aRqm8J3A" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/Indicoder.png"></a>
+  </div>
+  <div class="col-xs-12 col-sm-9">
+        <h2>Indicoder: Social Media Partner</h2>
+        <p>
+	Indicoder is one of the most popular cubing and tech youtube channels in the country. Over a very short period of time, the channel has reached almost 100K subscribers. This is the go-to destination for anyone interested to know more about how to solve a cube. With over 6 Million views, the tutorial on how to solve a 3x3 in Hindi is one of the most popular cubing videos on youtube and has helped a lot of people kickstart their cubing journey.	
+</p>
+        <p>
+            <a href="https://www.youtube.com/channel/UCHk5rERcW5h_4C7aRqm8J3A" target="_blank">Click here to visit Indicoder</a>!
+        </p>
+  </div>
+</div>
+<div class="row justify-content-center">
   <div class="col-xs-12 col-sm-3">
       <!--  <a href="https://www.duexpress.in/" target="_blank"><img class="img-responsive" style="margin-bottom: 3em; margin-top: 2em; margin-left: 2em;  height: 16em;"  src="../img/sponsors/DUExpress.png"></a>-->
   </div>
