fea9be5a4047370a4f218bc42e7e0a147479e51a
Piyush <piyushp761@users.noreply.github.com>
2019-04-07 20:29:36 +0530

    Changed channel to indiannationals2019

--- livestream/index.php
+++ livestream/index.php
@@ -2,10 +2,8 @@
 include('../header.php')
 ?>
 
-<iframe src="https://player.twitch.tv/?channel=Dhead_69" style="margin-top:7%; margin-bottom: 1%;" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+<iframe src="https://player.twitch.tv/?channel=indiannationals2019" style="margin-top:7%; margin-bottom: 1%;" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
 
 <?php
 include('../footer.php')
 ?>
-
-
